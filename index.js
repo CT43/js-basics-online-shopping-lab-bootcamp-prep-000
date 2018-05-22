f3fbd66ca0d102bc2d1a1cde5f37b292a1025f28
christian.thomas.2894@gmail.com <christian.thomas.2894@gmail.com>
2018-05-22 03:08:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ function placeOrder(cardNumber) {
     return `Sorry, we don't have a credit card on file for you.`
   } else {
     return cart = [];
-    
+    `Your total cost is $${sumToCharge}, which will be charged to the card ${cardNumber}.`
   }
 }
 
