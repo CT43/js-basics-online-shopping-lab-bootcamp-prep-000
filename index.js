b2ca2d4f3541948242fd0b50eb6b9275c78ebf3d
christian.thomas.2894@gmail.com <christian.thomas.2894@gmail.com>
2018-05-22 02:50:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -45,9 +45,6 @@ function total() {
   
 function removeFromCart(item) {
  var itemGone = item
- for (var i = 0; i < cart.length; i++){
-   if (cart[i].itemName !== itemGone) { return 'That item is not in your cart.'
- }}
  
  for (var i = 0; i < cart.length; i++)
    if (cart[i].itemName === itemGone) {  
