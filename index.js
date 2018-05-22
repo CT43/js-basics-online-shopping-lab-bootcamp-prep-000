d1e0d601bc242d2303fe74e8a46d6fdfff694c55
christian.thomas.2894@gmail.com <christian.thomas.2894@gmail.com>
2018-05-22 02:38:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -46,8 +46,8 @@ function total() {
 function removeFromCart(item) {
  var itemGone = item;
  for (var i = 0; i < cart.length; i++)
-   if cart[i].itemName === itemGone ?  
-     cart.splice(i, 1): 
+    return cart[i].itemName === itemGone ?  
+     cart.splice(i, 1): 'That item is not in your cart' }
 
  
 
