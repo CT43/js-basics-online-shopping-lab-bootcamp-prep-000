15dbee2ef13880cfbaba3a9ef72aff10718f5385
christian.thomas.2894@gmail.com <christian.thomas.2894@gmail.com>
2018-05-22 03:04:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -51,14 +51,10 @@ function removeFromCart(item) {
      cart.splice(i, 1)}}
  if (checkCart() === true){}
  else { return 'That item is not in your cart.'}
-   
 }
 
- 
-
-
 function placeOrder(cardNumber) {
-  // write your code here
+
 }
 
 
